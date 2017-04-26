c78125dd3fea79cfcc3118dcf8e73db56aaff7bf
CHRIS <chris.shandgost@gmail.com>
2017-04-26 11:02:31 +1000

    Initial commit

---
+++ Snake/src/GameMain.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using SwinGameSDK;
+
+namespace MyGame
+{
+    public class GameMain
+    {
+        public static void Main()
+        {
+            //Open the game window
+            SwinGame.OpenGraphicsWindow("Snake", 800, 600);
+            SwinGame.ShowSwinGameSplashScreen();
+
+            //Run the game loop
+            do
+            {
+                //Fetch the next batch of UI interaction
+                SwinGame.ProcessEvents();
+
+                //Clear the screen and draw the framerate
+                SwinGame.ClearScreen(Color.White);
+                SwinGame.DrawFramerate(0, 0);
+
+                //Draw onto the screen
+                SwinGame.RefreshScreen(60);
+            } while (!SwinGame.WindowCloseRequested());
+        }
+        
+    }
+}
---
+++ Snake/src/GameState.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// Handles the various states of the game.
+/// Used to manipulate input handling based
+/// on the current state of the game.
+/// </summary>
+
+public enum GameState
+{
+    MainMenu,
+    InGame,
+    GameOver,
+    Quitting
+}
