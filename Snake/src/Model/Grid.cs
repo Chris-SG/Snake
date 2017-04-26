f5cd4384ce9e8e72fdc13d26d75cb5933893d172
CHRIS <chris.shandgost@gmail.com>
2017-04-26 12:09:46 +1000

    Update grid

--- Snake/src/Model/Grid.cs
+++ Snake/src/Model/Grid.cs
@@ -6,19 +6,38 @@ using System.Threading.Tasks;
 
 using SwinGameSDK;
 
-namespace MyGame.src
+namespace Snake
 {
+    /// <summary>
+    /// A grid is the map in which the Snake game is played.
+    /// It can include a Snake and food.
+    /// </summary>
     class Grid
     {
         private Point2D _gridSize;
         private char[,] _grid;
 
+        /// <summary>
+        /// The constructor can use any given grid size.
+        /// </summary>
+        /// <param name="gridSize">The size of the grid.</param>
         Grid(Point2D gridSize)
         {
             _gridSize = gridSize;
             _grid = new char[Width, Height];
+
+            for(int i = 0; i < Width; i++)
+            {
+                for(int j = 0; j < Height; j++)
+                {
+                    _grid[i, j] = (char)0;
+                }
+            }
         }
 
+        /// <summary>
+        /// Width of the grid.
+        /// </summary>
         public int Width
         {
             get
@@ -26,7 +45,10 @@ namespace MyGame.src
                 return Convert.ToInt32(_gridSize.X);
             }
         }
-
+        
+        /// <summary>
+        /// Height of the grid.
+        /// </summary>
         public int Height
         {
             get
@@ -34,6 +56,12 @@ namespace MyGame.src
                 return Convert.ToInt32(_gridSize.Y);
             }
         }
+
+        /// <summary>
+        /// View a particular location on the grid.
+        /// </summary>
+        /// <param name="pos">Position to investigate.</param>
+        /// <returns>Value from given position.</returns>
         public char View(Point2D pos)
         {
             return _grid[Convert.ToInt32(pos.X), Convert.ToInt32(pos.Y)];
