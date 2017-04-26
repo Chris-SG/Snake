149129374be3a1682280898f5ce249253f7bfff3
CHRIS <chris.shandgost@gmail.com>
2017-04-26 11:56:20 +1000

    Initial grid implementation

---
+++ Snake/src/Model/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SwinGameSDK;
+
+namespace MyGame.src
+{
+    class Grid
+    {
+        private Point2D _gridSize;
+        private char[,] _grid;
+
+        Grid(Point2D gridSize)
+        {
+            _gridSize = gridSize;
+            _grid = new char[Width, Height];
+        }
+
+        public int Width
+        {
+            get
+            {
+                return Convert.ToInt32(_gridSize.X);
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return Convert.ToInt32(_gridSize.Y);
+            }
+        }
+        public char View(Point2D pos)
+        {
+            return _grid[Convert.ToInt32(pos.X), Convert.ToInt32(pos.Y)];
+        }
+        
+    }
+}
