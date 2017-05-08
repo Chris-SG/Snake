91d586acfb7c71a37a0926eb710a8e32c380d125
Harry <phamhuuhuy93@gmail.com>
2017-05-08 11:36:39 +1000

    UnitTest

---
+++ MyGameWinTests/src/SnakeTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyGameWinTests.src
+{
+    [TestClass]
+    public class SnakeTest
+    {
+        Snake s = new Snake();
+
+        [TestMethod]
+        public void SpawnTest()
+        {
+            s.Spawn();
+            Assert.AreEqual(2, s.length); // an initial snake equals 2 that includes head and body of the snake
+        }
+
+        [TestMethod]
+        public void EatFoodTest()
+        {
+            s.EatFood();
+            Assert.AreEqual(3, s.length);
+        }
+    }
+}
