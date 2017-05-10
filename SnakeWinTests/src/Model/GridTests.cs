094b0a3f70572f9339011daecb67f6ecda6e61f7
NathanD19 <nathandavies19@yahoo.com.au>
2017-05-10 16:49:29 +1000

    snake object tests

--- SnakeWinTests/src/Model/GridTests.cs
+++ SnakeWinTests/src/Model/GridTests.cs
@@ -12,10 +12,13 @@ namespace Snake.Tests
     public class GridTests
     {
         [TestMethod()]
-        public void GridTest()
+        public void GridSizeTest()
         {
             
-            Grid grid = new Grid();
+            Grid grid = new Grid(new Tuple<int, int>(10, 10));
+
+            Assert.AreEqual(grid.Height, 10);
+            Assert.AreEqual(grid.Width, 10);
         }
     }
 }
