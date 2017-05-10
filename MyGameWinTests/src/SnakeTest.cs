6e8379b5af45ddfc69228d61a3f377e406002bca
NathanD19 <nathandavies19@yahoo.com.au>
2017-05-10 12:04:38 +1000

    first commit test

---
+++ MyGameWinTests/src/SnakeTest.cs
@@ -0,0 +1,19 @@
+using Snake;
+using System;
+using NUnit.Framework;
+
+namespace Snake.Tests
+{
+    [TestFixture()]
+    public class Snake
+    {
+        [Test()]
+        public void LengthIncrease()
+        {
+        	Snake testSnake = new Snake();
+
+
+            Assert.Fail();
+        }
+    }
+}
