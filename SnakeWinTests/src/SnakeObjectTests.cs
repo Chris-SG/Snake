67d61d81ca8ffd0be8df04732a7e53e271a76025
NathanD19 <nathandavies19@yahoo.com.au>
2017-05-10 15:28:24 +1000

    snake test

---
+++ SnakeWinTests/src/SnakeObjectTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Snake
+{
+    [TestClass]
+    public class SnakeObjectTests
+    {
+        [TestMethod]
+        public void LengthTest()
+        {
+            SnakeObject snake = new SnakeObject(6, 6);
+            snake.Grow = true;
+
+            for(int i = 0; i < 60; i++)
+            {
+                snake.Movement();
+            }
+
+            Assert.AreEqual(2, snake.Length);
+
+        }
+    }
+}
