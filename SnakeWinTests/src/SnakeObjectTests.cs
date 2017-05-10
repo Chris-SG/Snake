396a5aa8ffcfd35097afaececfa2d0702f337cc8
NathanD19 <nathandavies19@yahoo.com.au>
2017-05-10 16:13:04 +1000

    snake object tests

--- SnakeWinTests/src/SnakeObjectTests.cs
+++ SnakeWinTests/src/SnakeObjectTests.cs
@@ -22,12 +22,60 @@ namespace Snake
         }
 
         [TestMethod]
-        public void MovementTest()
+        public void SpawnTest()
         {
             SnakeObject snake = new SnakeObject(6, 6);
             
             Assert.IsTrue(snake.X == 3 && snake.Y == 3);
+        }
+
+        [TestMethod]
+        public void MovmentTest()
+        {
+            SnakeObject snake = new SnakeObject(6, 6);
+            snake.Direction = SnakeDirection.Down;
+
+
+            for(int i = 0; i < 30; i++)
+                snake.Movement();
+
+            Assert.IsTrue(snake.X == 3 && snake.Y == 4);
 
         }
+
+        [TestMethod]
+        public void DirectionTest()
+        {
+            SnakeObject snake = new SnakeObject(6, 6);
+            snake.Direction = SnakeDirection.Left;
+
+            Assert.AreEqual(SnakeDirection.Left, snake.Direction);
+
+            snake.Direction = SnakeDirection.Up;
+
+            Assert.AreEqual(SnakeDirection.Up, snake.Direction);
+
+        }
+
+        [TestMethod]
+        public void ExtendedMovementTest()
+        {
+            SnakeObject snake = new SnakeObject(6, 6);
+            snake.Direction = SnakeDirection.Down;
+
+            for (int i = 0; i < 30; i++)
+                snake.Movement();
+
+            Assert.IsTrue(snake.X == 3 && snake.Y == 4);
+
+            snake.Direction = SnakeDirection.Left;
+
+            for (int i = 0; i < 30; i++)
+                snake.Movement();
+
+            Assert.IsTrue(snake.X == 2 && snake.Y == 4);
+
+        }
+
     }
 }
