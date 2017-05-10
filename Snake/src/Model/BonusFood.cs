335bb65f34cdde4c484061fef6736d84f9d21916
CHRIS <chris.shandgost@gmail.com>
2017-05-10 16:39:56 +1000

    Add food

---
+++ Snake/src/Model/BonusFood.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class BonusFood : Item
+	{
+		private int _lifetime;
+
+		public BonusFood(int x, int y, int aLifetime) : base(x, y)
+		{
+			_lifetime = aLifetime;
+		}
+	}
+}
