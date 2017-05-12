fe32fef03af8e960bbb22ac7301ad1d2aac22a96
NathanD19 <nathandavies19@yahoo.com.au>
2017-05-12 14:48:21 +1000

    item tests
    tests missing

---
+++ SnakeWinTests/src/Model/ItemTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Snake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Tests
+{
+    [TestClass()]
+    public class Itemtests
+    {
+        [TestMethod()]
+        public void FoodExist()
+        {
+            //items & foods are currently unaccesible
+        }
+    }
+}
