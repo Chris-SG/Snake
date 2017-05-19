613ce47530bbd6ce589459ad9cdf4cb1800479bb
Minura Gamage <minuradevin@gmail.com>
2017-05-19 16:31:48 +1000

    Add more unit tests for grid

--- SnakeWinTests/src/Model/GridTests.cs
+++ SnakeWinTests/src/Model/GridTests.cs
@@ -28,5 +28,32 @@ namespace Snake.Tests
             
             Assert.IsTrue(grid.SnakeObj.X == 5 && grid.SnakeObj.Y == 5);            
         }
+
+        [TestMethod()]
+        public void GridFullTest()
+        {
+            Grid grid = new Grid(new Tuple<int, int>(1, 1));
+            grid.CommenceGame();
+
+            Assert.IsTrue(grid.SnakeObj.X == 0 && grid.SnakeObj.Y == 0);
+        }
+
+        [TestMethod()]
+        public void GridItemDeploymentTest()
+        {
+            Grid grid = new Grid(new Tuple<int, int>(10, 10));
+            grid.CommenceGame();
+            //need to know the functions to deploy food
+        }
+
+        [TestMethod()]
+        public void GridItemDeploymentClashTest()
+        {
+            Grid grid = new Grid(new Tuple<int, int>(10, 10));
+            grid.CommenceGame();
+            //need to know the functions to deploy food
+            //check if there is a clash of items
+        }
+
     }
 }
