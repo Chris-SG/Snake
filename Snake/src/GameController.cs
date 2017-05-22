5b7c7122ae6858a910b18136714bf181ef5a8f82
CHRIS <chris.shandgost@gmail.com>
2017-05-22 13:08:48 +1000

    More drawing

--- Snake/src/GameController.cs
+++ Snake/src/GameController.cs
@@ -7,12 +7,14 @@ namespace Snake
     public static class GameController
     {
         private static readonly int SNAKE_PART_LENGTH = 25;
+		private static int SNAKE_DRAWING_OFFSET;
         private static Stack<GameState> _state;
 
         private static Grid _grid;
 
         static GameController()
         {
+			SNAKE_DRAWING_OFFSET = 50;
 			_state = new Stack<GameState>();
 			_state.Push(GameState.MainMenu);
         }
@@ -74,10 +76,19 @@ namespace Snake
         {
             foreach (var parts in s.SnakePos)
             {
-                SwinGame.FillRectangle(Color.Coral, parts.Item1 * SNAKE_PART_LENGTH, parts.Item2* SNAKE_PART_LENGTH, SNAKE_PART_LENGTH, SNAKE_PART_LENGTH);
-                SwinGame.DrawRectangle(Color.BlanchedAlmond, parts.Item1 * SNAKE_PART_LENGTH, parts.Item2 * SNAKE_PART_LENGTH, SNAKE_PART_LENGTH, SNAKE_PART_LENGTH);
+                SwinGame.FillRectangle(Color.Coral, parts.Item1 * SNAKE_PART_LENGTH + SNAKE_DRAWING_OFFSET, parts.Item2* SNAKE_PART_LENGTH + SNAKE_DRAWING_OFFSET, SNAKE_PART_LENGTH, SNAKE_PART_LENGTH);
+                SwinGame.DrawRectangle(Color.BlanchedAlmond, parts.Item1 * SNAKE_PART_LENGTH + SNAKE_DRAWING_OFFSET, parts.Item2 * SNAKE_PART_LENGTH + SNAKE_DRAWING_OFFSET, SNAKE_PART_LENGTH, SNAKE_PART_LENGTH);
             }
-        }
+			foreach(var itm in _grid.Items)
+			{
+				SwinGame.FillRectangle(Color.GreenYellow, itm.X * SNAKE_PART_LENGTH + SNAKE_DRAWING_OFFSET, itm.Y * SNAKE_PART_LENGTH + SNAKE_DRAWING_OFFSET, SNAKE_PART_LENGTH, SNAKE_PART_LENGTH);
+			}
+
+			SwinGame.FillRectangle(Color.Black, SNAKE_DRAWING_OFFSET-SNAKE_PART_LENGTH, SNAKE_DRAWING_OFFSET-SNAKE_PART_LENGTH, (_grid.Width+2) * SNAKE_PART_LENGTH, SNAKE_PART_LENGTH);
+			SwinGame.FillRectangle(Color.Black, SNAKE_DRAWING_OFFSET-SNAKE_PART_LENGTH, SNAKE_DRAWING_OFFSET-SNAKE_PART_LENGTH, SNAKE_PART_LENGTH, (_grid.Height+2) * SNAKE_PART_LENGTH);
+			SwinGame.FillRectangle(Color.Black, SNAKE_DRAWING_OFFSET-SNAKE_PART_LENGTH, _grid.Height * SNAKE_PART_LENGTH + SNAKE_DRAWING_OFFSET, (_grid.Width+2) * SNAKE_PART_LENGTH, SNAKE_PART_LENGTH);
+			SwinGame.FillRectangle(Color.Black, _grid.Width * SNAKE_PART_LENGTH + SNAKE_DRAWING_OFFSET, SNAKE_DRAWING_OFFSET- SNAKE_PART_LENGTH, SNAKE_PART_LENGTH, (_grid.Height+2) * SNAKE_PART_LENGTH);
+		}
 
         public static void DrawGame()
         {
